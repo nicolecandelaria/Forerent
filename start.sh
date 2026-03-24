6785b6a38ba4dca0ef6540cb85bc0692e1b41f6c
jeh <abrerajohnrey@gmail.com>
2026-03-24 14:41:35 +0800

    seed

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 chmod -R 775 /var/www/storage
 chmod -R 775 /var/www/bootstrap/cache
-php artisan migrate:fresh --seed --force
+php artisan migrate:fresh --force
 php artisan config:cache
 php artisan storage:link
+
+# Seed in background so supervisord starts immediately
+php artisan db:seed --force &
+
+# Start nginx and php-fpm immediately
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
