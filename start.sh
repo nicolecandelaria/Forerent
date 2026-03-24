ea9046970e53374b4c58d5392da658c9077481a3
jeh <abrerajohnrey@gmail.com>
2026-03-24 11:33:13 +0800

    seed

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 php artisan migrate --force
+php artisan db:seed --force
 php artisan config:cache
 php artisan storage:link
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
