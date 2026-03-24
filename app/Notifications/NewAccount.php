e3c4d76c4618269d86708479f322765f4dfc5103
root <root@DESKTOP-3D4CRC2.localdomain>
2026-03-24 17:20:33 +0800

    fix: new acccount

--- app/Notifications/NewAccount.php
+++ app/Notifications/NewAccount.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class NewAccount extends Notification
+class NewAccount extends Notification implements ShouldQueue
 {
     use Queueable;
 
