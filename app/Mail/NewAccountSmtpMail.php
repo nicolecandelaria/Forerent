71f442aa2e6dfc8827c855aeaa3e168bd6957bfe
nicolecandelaria <ncandelaria.a12241579@umak.edu.ph>
2026-04-03 20:38:43 +0800

    fix: fast receiving of email - new account smtp mail

--- app/Mail/NewAccountSmtpMail.php
+++ app/Mail/NewAccountSmtpMail.php
@@ -3,10 +3,11 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class NewAccountSmtpMail extends Mailable
+class NewAccountSmtpMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
